61d8a144e53bb372c77c0773b42fe7f420c77b4f
Simone Lopes <sisilopes20@gmail.com>
2023-05-23 07:32:20 +0100

    create login page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,17 +12,16 @@ export const metadata: Metadata = {
 
 export default function Home() {
   return (
-    <>
-      <div>
-        <Image src={logo} alt='Logo Pizzaria Dev' />
-        <div className={styles.login}>
-          <form>
-            <Input placeholder='Digite seu email' type='text' />
-            <Input placeholder='Sua senha' type='password' />
-            <Button type="submit" loading={true}>Acessar</Button>
-          </form>
-        </div>
+    <div className={styles.containerCenter}>
+      <Image src={logo} alt='Logo Pizzaria Dev' />
+      <div className={styles.login}>
+        <form>
+          <Input placeholder='Digite seu email' type='text' />
+          <Input placeholder='Sua senha' type='password' />
+          <Button type="submit" loading={true}>Acessar</Button>
+        </form>
+        <a className={styles.text}>Não possui uma conta? Cadastre-se</a>
       </div>
-    </>
+    </div>
   )
 }
